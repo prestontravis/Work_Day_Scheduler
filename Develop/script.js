8dcfe13f6011874102e5722c9fbab5a4a80d4816
prestontravis <119766277+prestontravis@users.noreply.github.com>
2023-04-29 21:08:00 -0400

    added javascript code

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,39 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Get the current time and format it to be displayed
+var currentTime = dayjs().format("dddd, MMMM D, YYYY");
+$("#currentDay").text(currentTime);
+
+// Check the current time against each time block and add the appropriate class
+function checkTime() {
+  var currentHour = dayjs().hour();
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+}
+
+// Call checkTime to update the colors of the time blocks
+checkTime();
+
+// Add event listener to the save button to store the user's input in local storage
+$(".saveBtn").on("click", function () {
+  var time = $(this).parent().attr("id");
+  var description = $(this).siblings(".description").val();
+  localStorage.setItem(time, description);
 });
+  
+// Retrieve the user's input from local storage and display it in the appropriate time block
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
